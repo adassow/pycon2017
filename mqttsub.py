83f7653774b54a1abb9c2dd3522d4cac28612585
Adassow <adassow@gmail.com>
2017-08-16 19:53:57 +0200

    init

---
+++ mqttsub.py
@@ -0,0 +1,16 @@
+import network
+from umqttsimple import MQTTClient
+
+wifi = network.WLAN(network.STA_IF)
+
+wifi.active(True)
+wifi.connect("dl", "dl")
+while not wifi.isconnected():
+	pass
+
+
+
+c = MQTTClient("my_esp", "mymqtt.com")
+c.connect()
+c.publish(c"foo_topic", "Hello World!!!")
+c.disconnect()
